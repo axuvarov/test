ef2b59e016b647d7c0873bcee2fb5ecdd64a827f
Александр Уваров <axuvarov@gmail.com>
2017-06-24 01:00:53 +0300

    comment

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+//Тест
 class DashboardController extends Controller
 {
     //Метот показа шаблона
